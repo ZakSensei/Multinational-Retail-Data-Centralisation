db0d2cd54040565043642a2667cfc0558e12b89a
Zak Ahmed <sakariye192@gmail.com>
2023-12-05 23:49:34 +0000

    stashing changes

--- data_extraction.py
+++ data_extraction.py
@@ -1,9 +1,10 @@
 import pandas as pd
+
 class DataExtractor():
     """This is a utility class which helps extract data from different data sources including: 
         CSV files, an API and an S3 bucket."""
 
     def read_rds_table(self, table_name, engine):
-        df = pd.read_sql_table(table_name, engine) 
+        df = pd.read_sql_table(table_name, engine)
         return df
     
